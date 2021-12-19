33171c7b43cd5bec773bf09ef94f3b509d14d738
HarmanieH <hhubbard@colum.edu>
2021-12-19 01:08:42 -0800

    CharacterMovement2

--- FinalAssignment/Assets/Sunnyland/Scripts/PlayerMovement.cs
+++ FinalAssignment/Assets/Sunnyland/Scripts/PlayerMovement.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
     }
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+
+
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+
         transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
